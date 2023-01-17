cf9c5420f9847ed53cf3653ee9820392bfdbc91b
Pol Vallverdu <pvallverdualcaide@gmail.com>
2023-01-17 23:39:58 +0100

    Fixed some stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 // TODO: ESTO ES LO MAS GUARRO QUE HE PROGRAMADO EN TODA MI VIDA. EL DIA QUE ME ABURRA O LOS SERVIDORES PETEN, TOCA REESCRIBIR ESTO :D
 
-use std::{fs::File, io::{Read, Seek, SeekFrom, Write}};
 use anyhow::Result;
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom, Write},
+};
 
 pub enum Channels {
     Mono = 0,
@@ -29,7 +32,7 @@ impl Clone for Track {
 
 pub struct PackedTrack {
     pub track: Track,
-    pub data: Vec<u8>,
+    pub data: Vec<Vec<u8>>,
 }
 
 pub struct RopusReader {
@@ -38,8 +41,8 @@ pub struct RopusReader {
     pub samplerate: i32,
     pub tracks_num: i32,
     pub tracks: Vec<Track>,
-    
-    packs_num: i32,
+
+    pub packs_num: i32,
     pack_seek: Vec<i32>,
 
     current_pack: i32,
@@ -51,19 +54,19 @@ impl RopusReader {
 
         // Get version as int
         let old_p = pointer;
-        pointer+=4;
+        pointer += 4;
         let num = &buf[old_p..pointer];
         let version = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
 
         // Get samplerate as int
         let old_p = pointer;
-        pointer+=4;
+        pointer += 4;
         let num = &buf[old_p..pointer];
         let samplerate = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
 
         // Get tracks_num as int
         let old_p = pointer;
-        pointer+=4;
+        pointer += 4;
         let num = &buf[old_p..pointer];
         let tracks_num = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
 
@@ -84,7 +87,7 @@ impl RopusReader {
 
         // Get packs_num
         let old_p = pointer;
-        pointer+=4;
+        pointer += 4;
         let num = &buf[old_p..pointer];
         let packs_num = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
 
@@ -92,7 +95,7 @@ impl RopusReader {
         let mut pack_seek = Vec::new();
         for _ in 0..packs_num {
             let old_p = pointer;
-            pointer+=4;
+            pointer += 4;
             let pack = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
             pack_seek.push(pack);
         }
@@ -110,29 +113,36 @@ impl RopusReader {
 
     pub fn read_pack(&mut self, buf: Vec<u8>) -> Vec<PackedTrack> {
         let mut packed_tracks = Vec::new();
+        let mut track_packets: Vec<Vec<Vec<u8>>> = vec![Vec::new(); self.tracks_num as usize];
         let mut pointer = 0;
 
-        // Get the size of each track
-        let mut track_sizes = Vec::new();
-        for _ in 0..self.tracks_num {
-            let old_p = pointer;
-            pointer+=4;
-            let num = &buf[old_p..pointer];
-            let track_size = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
-            track_sizes.push(track_size);
+        while buf.len() > 0 {
+            // Get the size of each track
+            let mut track_sizes = Vec::new();
+            for _ in 0..self.tracks_num {
+                let old_p = pointer;
+                pointer += 4;
+                let num = &buf[old_p..pointer];
+                let track_size = i32::from_be_bytes([num[0], num[1], num[2], num[3]]);
+                track_sizes.push(track_size);
+            }
+
+            // Get the data for each track
+            for i in 0..self.tracks_num as usize {
+                let size = track_sizes[i as usize] as usize;
+
+                let old_p = pointer;
+                pointer += size;
+
+                let mut data = buf[old_p..pointer].to_vec();
+                track_packets[i].push(data);
+            }
         }
 
-        // Get the data for each track
-        for i in 0..self.tracks_num {
-            let size = track_sizes[i as usize] as usize;
-
-            let old_p = pointer;
-            pointer+=size;
-
-            let mut data = buf[old_p..pointer].to_vec();
+        for i in 0..self.tracks_num as usize {
             packed_tracks.push(PackedTrack {
-                track: self.tracks[i as usize].clone(),
-                data,
+                track: self.tracks[i].clone(),
+                data: track_packets[i].clone(),
             });
         }
 
@@ -140,7 +150,6 @@ impl RopusReader {
 
         packed_tracks
     }
-
 }
 
 pub struct RopusWriter {
@@ -149,7 +158,7 @@ pub struct RopusWriter {
     pub samplerate: i32,
     pub tracks_num: i32,
     pub tracks: Vec<Track>,
-    
+
     packs: Vec<Vec<PackedTrack>>,
 }
 
@@ -159,13 +168,17 @@ impl RopusWriter {
             version: 1,
             samplerate,
             tracks_num: tracks.len() as i32,
-            tracks: tracks.into_iter().map(|track| Track { channels: track }).collect(),
+            tracks: tracks
+                .into_iter()
+                .map(|track| Track { channels: track })
+                .collect(),
             packs: Vec::new(),
         }
     }
 
-    pub fn add_pack(&mut self, pack: Vec<Vec<u8>>) {
+    pub fn add_pack(&mut self, pack: Vec<Vec<Vec<u8>>>) {
         let mut packed_tracks = Vec::new();
+        
         for i in 0..self.tracks_num {
             packed_tracks.push(PackedTrack {
                 track: self.tracks[i as usize].clone(),
@@ -199,10 +212,16 @@ impl RopusWriter {
         header.extend_from_slice(&(self.packs.len() as i32).to_be_bytes());
 
         // Add pack_seek
+        // TODO: CHANGE
         let mut pack_seek = 0;
         for _ in 0..self.packs.len() {
             header.extend_from_slice(&(pack_seek as i32).to_be_bytes());
-            pack_seek += 4 + (4 * self.tracks_num as usize) + self.packs[0].iter().map(|track| track.data.len()).sum::<usize>();
+            pack_seek += 4
+                + (4 * self.tracks_num as usize)
+                + self.packs[0]
+                    .iter()
+                    .map(|track| track.data.len())
+                    .sum::<usize>();
         }
 
         header
@@ -212,14 +231,18 @@ impl RopusWriter {
         let mut packs = Vec::new();
 
         for pack in &self.packs {
-            // Add the size of each track
-            for track in pack {
-                packs.extend_from_slice(&(track.data.len() as i32).to_be_bytes());
-            }
+            let opus_pack_len = pack[0].data.len();
 
-            // Add the data for each track
-            for track in pack {
-                packs.extend_from_slice(&track.data);
+            for i in 0..opus_pack_len {
+                // Add the size of each track
+                for track in pack {
+                    packs.extend_from_slice(&(track.data.len() as i32).to_be_bytes());
+                }
+
+                // Add the data for each track
+                for track in pack {
+                    packs.extend_from_slice(&track.data[i]);
+                }
             }
         }
 
@@ -230,15 +253,29 @@ impl RopusWriter {
         let mut file = File::create(path)?;
 
         let header = self.get_header();
-        file.write(&header.len().to_be_bytes());
-        file.write_all(&header)?;
-        file.write_all(&self.get_packs())?;
+        let header_size = (header.len() as i32).to_be_bytes();
+        let mut len = file.write(&header_size)?;
+        println!("Header size: {}", len);
+        len = file.write(header.as_slice())?;
+        println!("Header: {:?}", len);
+        len = file.write(&self.get_packs())?;
+        println!("Packs: {:?}", len);
+
+        file.flush()?;
 
         Ok(())
     }
 }
 
 pub struct RopusFileReader {
+    /* FOR EASY ACCESS */
+    pub version: i32,
+
+    pub samplerate: i32,
+    pub tracks_num: i32,
+    //pub tracks: Vec<Track>,
+    pub packs_num: i32,
+    /* FOR EASY ACCESS */
     pub reader: RopusReader,
 
     file: File,
@@ -251,7 +288,9 @@ impl RopusFileReader {
         // Get header size as int
         let mut header_size = [0u8; 4];
         file.read(&mut header_size)?;
+        println!("Header size: {:?}", header_size);
         let header_size = i32::from_be_bytes(header_size);
+        println!("Header size: {}", header_size);
 
         // Get header as Vec<u8>
         let mut header = vec![0u8; header_size as usize];
@@ -260,6 +299,11 @@ impl RopusFileReader {
         let ropus_reader = RopusReader::load(header)?;
 
         Ok(RopusFileReader {
+            version: ropus_reader.version,
+            samplerate: ropus_reader.samplerate,
+            tracks_num: ropus_reader.tracks_num,
+            //tracks: ropus_reader.tracks,
+            packs_num: ropus_reader.packs_num,
             reader: ropus_reader,
             file,
         })
@@ -335,7 +379,6 @@ impl RopusFileReader {
 
         self.reader.read_pack(buf)
     }
-
 }
 
 // pub struct RopusFileWriter
